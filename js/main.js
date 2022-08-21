7279517532272ecf13e5067f38956037758bd01b
harklz <zcae1020@naver.com>
2022-08-21 19:20:56 +0000

    search complete

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
+let center = new kakao.maps.LatLng(33.450701, 126.570667);
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
   mapOption = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    center: center, // 지도의 중심좌표
     level: 3 // 지도의 확대 레벨
   };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
+
+function OC() {
+  console.log(5);
+  let sr = document.getElementById("searchresult");
+  let sbg = document.getElementById("searchbackground");
+
+  sr.style.display = "none";
+  sbg.style.display = "none";
+}
+
+mapContainer.onclick = OC;
--- js/search.js
+++ js/search.js
@@ -1,11 +1,112 @@
-input = document.getElementById("searchbar");
+searchbar = document.getElementById("searchbar");
+searchtext = document.getElementById("searchtext");
+searchresult = document.getElementById("searchresult");
+sbackground = document.getElementById("searchbackground");
 
+let prevMarker;
 
-
-function search() {
-  input.submit();
+function submitHandler(event) {
+  sbackground.style.display = "block";
+  searchresult.style.display = "block";
+  event.preventDefault();
+  search(searchtext.value);
+  searchtext.value = '';
 }
 
-fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
-  console.log(response)
-);
+async function search(text) {
+  let url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query=' + encodeURIComponent(text);
+  let options = {
+    method: 'GET',
+    headers: {
+      "Authorization": "KakaoAK edd84ee3d8c9626a92ab5fbd2ab93622"
+    }
+  }
+
+  response = await fetch(url, options);
+  json = await response.json();
+
+  if (response.ok == false) {
+    alert("네트워크 오류입니다.");
+    return;
+  }
+  if (json.meta.total_count == 0) {
+    alert("검색결과가 없습니다.");
+    return;
+  }
+
+  display(json);
+}
+
+function display(results) {
+  markers = [];
+  bounds = new kakao.maps.LatLngBounds();
+
+  delList();
+
+  if (results.meta.total_count == 0) {
+    alert("검색결과가 없습니다.");
+    return;
+  }
+
+  for (idx in results.documents) {
+    data = results.documents[idx];
+    placePosition = new kakao.maps.LatLng(data.y, data.x);
+
+    bounds.extend(new kakao.maps.LatLng(data.y, data.x));
+
+    searchresult.appendChild(createList(idx + 1, data));
+    searchresult.appendChild(document.createElement("br"));
+
+  }
+
+  map.setBounds(bounds);
+}
+
+function createList(idx, data) {
+  let d = document.createElement("div");
+  let li1 = document.createElement("li");
+  let li2 = document.createElement("li");
+
+  d.id = "searchlist" + idx;
+  li1.innerText = data.place_name;
+  li2.innerText = data.address_name;
+  li2.style = "font-size:12px;color: rgb(134, 134, 147);";
+
+  d.appendChild(li1);
+  d.appendChild(li2);
+
+  d.addEventListener("mouseover", function() {
+    d.style = "background-color:#ecedd8";
+  });
+  d.addEventListener("mouseout", function() {
+    d.style = "background-color:white";
+  });
+  d.addEventListener("click", function() {
+    if (prevMarker)
+      prevMarker.setMap(null);
+
+    placePosition = new kakao.maps.LatLng(data.y, data.x);
+    marker = new kakao.maps.Marker({
+      position: placePosition,
+      clickable: true
+    });
+    marker.setMap(map);
+    prevMarker = marker;
+    map.setCenter(placePosition);
+    map.setLevel(3);
+
+    kakao.maps.event.addListener(marker, 'click', function() {
+      marker.setMap(null);
+    });
+  })
+
+  return d;
+}
+
+function delList() {
+  while (searchresult.firstChild) {
+    searchresult.removeChild(searchresult.firstChild);
+  }
+}
+
+searchbar.addEventListener("submit", submitHandler);
